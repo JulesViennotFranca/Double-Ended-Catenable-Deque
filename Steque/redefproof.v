48160b0962a3bc20c3baa67962d5b6464de414f5
Jules Viennot <jules.viennot@gmail.com>
2024-05-17 17:21:27 +0200

    Directly seq

--- Steque/redefproof.v
+++ Steque/redefproof.v
@@ -126,6 +126,112 @@ prefix'_seq (Pre2 a b) := [a] ++ [b];
 prefix'_seq (Pre3 a b c) := [a] ++ [b] ++ [c];
 prefix'_seq (Pre4 a b c d e) := [a] ++ [b] ++ [c] ++ [d] ++ deque_seq e.
 
+Section test.
+
+  Fixpoint element_seq {A lvl} (e : element A lvl) : list A :=
+    let fix prodN_seq {lvle lvlp C} (p : prodN (element A lvle) lvlp) : list A :=
+      match p with 
+      | prodZ e => element_seq e
+      | prodS p1 p2 => prodN_seq p1 ++ prodN_seq p2
+      end in
+    let buffer_seq {lvle lvlp C} (b : buffer (element A lvle) lvlp C) : list A :=
+      match b with 
+      | B0 => []
+      | B1 p1 => prodN_seq p1
+      | B2 p1 p2 => prodN_seq p1 ++ prodN_seq p2 
+      | B3 p1 p2 p3 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3
+      | B4 p1 p2 p3 p4 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4
+      | B5 p1 p2 p3 p4 p5 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4 ++ prodN_seq p5
+      end in 
+    let fix dpacket_seq {lvle lvlp len C} (pkt : lvl_packet (element A lvle) lvlp len C) : list A :=
+      match pkt with 
+      | lvlproof.Hole => []
+      | lvlproof.Triple p pkt s _ => buffer_seq p ++ dpacket_seq pkt ++ buffer_seq s 
+      end in
+    let fix cdeque_seq {lvle lvlp C} (cd : lvl_cdeque (element A lvle) lvlp C) : list A :=
+      match cd with
+      | lvlproof.Small b => buffer_seq b
+      | lvlproof.Big pkt cd _ _ => dpacket_seq pkt ++ cdeque_seq cd
+      end in
+    let prefix_seq {lvl C} (p : prefix A lvl C) : list A :=
+      match p with 
+      | Pre2 e1 e2 => element_seq e1 ++ element_seq e2 
+      | Pre3 e1 e2 e3 => element_seq e1 ++ element_seq e2 ++ element_seq e3
+      | Pre4 e1 e2 e3 e4 (lvlproof.T d) => 
+        element_seq e1 ++ element_seq e2 ++ element_seq e3 ++ element_seq e4 ++ cdeque_seq d
+      end in
+    match e with 
+    | ZElt a => [a]
+    | SElt p cs => prefix_seq p ++ csteque_seq cs 
+    end
+  
+  with packet_seq {A lvl len C} (pkt : packet A lvl len C) : list A :=
+    let fix prodN_seq {lvle lvlp C} (p : prodN (element A lvle) lvlp) : list A :=
+      match p with 
+      | prodZ e => element_seq e
+      | prodS p1 p2 => prodN_seq p1 ++ prodN_seq p2
+      end in
+    let buffer_seq {lvle lvlp C} (b : buffer (element A lvle) lvlp C) : list A :=
+      match b with 
+      | B0 => []
+      | B1 p1 => prodN_seq p1
+      | B2 p1 p2 => prodN_seq p1 ++ prodN_seq p2 
+      | B3 p1 p2 p3 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3
+      | B4 p1 p2 p3 p4 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4
+      | B5 p1 p2 p3 p4 p5 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4 ++ prodN_seq p5
+      end in 
+    let fix dpacket_seq {lvle lvlp len C} (pkt : lvl_packet (element A lvle) lvlp len C) : list A :=
+      match pkt with 
+      | lvlproof.Hole => []
+      | lvlproof.Triple p pkt s _ => buffer_seq p ++ dpacket_seq pkt ++ buffer_seq s 
+      end in
+    let fix cdeque_seq {lvle lvlp C} (cd : lvl_cdeque (element A lvle) lvlp C) : list A :=
+      match cd with
+      | lvlproof.Small b => buffer_seq b
+      | lvlproof.Big pkt cd _ _ => dpacket_seq pkt ++ cdeque_seq cd
+      end in
+    let prefix_seq {lvl C} (p : prefix A lvl C) : list A :=
+      match p with 
+      | Pre2 e1 e2 => element_seq e1 ++ element_seq e2 
+      | Pre3 e1 e2 e3 => element_seq e1 ++ element_seq e2 ++ element_seq e3
+      | Pre4 e1 e2 e3 e4 (lvlproof.T d) => 
+        element_seq e1 ++ element_seq e2 ++ element_seq e3 ++ element_seq e4 ++ cdeque_seq d
+      end in
+    match pkt with
+    | Hole => []
+    | Triple p pkt (Suff (lvlproof.T d)) => prefix_seq p ++ packet_seq pkt ++ cdeque_seq d
+    end
+
+  with csteque_seq {A lvl C} (cs : csteque A lvl C) : list A :=
+    let fix prodN_seq {lvle lvlp C} (p : prodN (element A lvle) lvlp) : list A :=
+      match p with 
+      | prodZ e => element_seq e
+      | prodS p1 p2 => prodN_seq p1 ++ prodN_seq p2
+      end in
+    let buffer_seq {lvle lvlp C} (b : buffer (element A lvle) lvlp C) : list A :=
+      match b with 
+      | B0 => []
+      | B1 p1 => prodN_seq p1
+      | B2 p1 p2 => prodN_seq p1 ++ prodN_seq p2 
+      | B3 p1 p2 p3 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3
+      | B4 p1 p2 p3 p4 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4
+      | B5 p1 p2 p3 p4 p5 => prodN_seq p1 ++ prodN_seq p2 ++ prodN_seq p3 ++ prodN_seq p4 ++ prodN_seq p5
+      end in 
+    let fix dpacket_seq {lvle lvlp len C} (pkt : lvl_packet (element A lvle) lvlp len C) : list A :=
+      match pkt with 
+      | lvlproof.Hole => []
+      | lvlproof.Triple p pkt s _ => buffer_seq p ++ dpacket_seq pkt ++ buffer_seq s 
+      end in
+    let fix cdeque_seq {lvle lvlp C} (cd : lvl_cdeque (element A lvle) lvlp C) : list A :=
+      match cd with
+      | lvlproof.Small b => buffer_seq b
+      | lvlproof.Big pkt cd _ _ => dpacket_seq pkt ++ cdeque_seq cd
+      end in
+    match cs with 
+    | Small (Suff (lvlproof.T d)) => cdeque_seq d
+    | Big pkt cs _ _ => packet_seq pkt ++ csteque_seq cs 
+    end.
+
 Section rect.
 
   Variable A : Type.
@@ -137,85 +243,93 @@ Section rect.
   Variable Pdeque : forall{B}, deque B -> Type.
   
   Print deque_rectP.
-
-  Variable Pprefix'
+  Print cdeque_rectP.
 
   Variable Pelement : forall {lvl : nat}, element A lvl -> Type.
   Variable Ppacket : forall {lvl len : nat} {C : color}, packet A lvl len C -> Type.
   Variable Pcsteque : forall {lvl : nat} {C : color}, csteque A lvl C -> Type.
 
-  Variable P_Pre2 : 
+  Variable Pprefix : forall {lvl : nat} {C : color}, prefix A lvl C -> Type.
+  Variable Psuffix : forall {lvl : nat}, suffix A lvl -> Type.
+
+  Variable P_Pre2 : forall {lvl : nat} {e1 e2 : element A lvl},
+    Pelement e1 -> Pelement e2 -> Pprefix (Pre2 e1 e2).
+  Variable P_Pre3 : forall {lvl : nat} {e1 e2 e3 : element A lvl},
+    Pelement e1 -> Pelement e2 -> Pelement e3 -> Pprefix (Pre3 e1 e2 e3).
+  Variable P_Pre4 : forall {lvl : nat} {e1 e2 e3 e4 : element A lvl} {d : deque (element A lvl)},
+    Pelement e1 -> Pelement e2 -> Pelement e3 -> Pelement e4 ->
+    Pdeque d ->
+    Pprefix (Pre4 e1 e2 e3 e4 d).
+
+  Variable P_Suff : forall {lvl : nat} {d : deque (element A lvl)},
+    Pdeque d -> Psuffix (Suff d).
 
   Variable P_ZElt : forall (a : A), Pelement (ZElt a).
-
   Variable P_SElt : 
     forall {lvl : nat} {C1 C2 : color} {p : prefix A lvl C1} {cs : csteque A (S lvl) C2},
-      (* list_rect (fun _ => Type) unit (fun e l tl => Pelement lvl e -> tl) (prefix'_seq p) -> *)
-      Forallt Pelement (prefix'_seq p) ->
+      Pprefix p ->
       Pcsteque cs ->
       Pelement (SElt p cs).
-  (* Arguments P_SElt {lvl C1 C2 p cs}. *)
 
   Variable P_Hole :
     forall {lvl : nat}, Ppacket (lvl := lvl) Hole.
-  (* Arguments P_Hole {lvl}. *)
-
   Variable P_Triple :
     forall {lvl len : nat} {Y : yellow_hue} {C : color}
            {p : prefix A lvl C} {pkt : packet A (S lvl) len (Mix NoGreen Y NoRed)} {s : suffix A lvl},
-      (* list_rect (fun _ => Type) unit (fun e l tl => Pelement lvl e -> tl) (prefix'_seq p) -> *)
-      Forallt Pelement (prefix'_seq p) ->
+      Pprefix p ->
       Ppacket pkt ->
-      (* list_rect (fun _ => Type) unit (fun e l tl => Pelement lvl e -> tl) (suffix'_seq s) -> *)
-      Forallt Pelement (suffix'_seq s) ->
+      Psuffix s ->
       Ppacket (Triple p pkt s).
-  (* Arguments P_Triple {lvl len Y C p pkt s}. *)
 
   Variable P_Small :
     forall {lvl : nat} {s : suffix A lvl}, 
-      (* list_rect (fun _ => Type) unit (fun e l tl => Pelement lvl e -> tl) (suffix'_seq s) -> *)
-      Forallt Pelement (suffix'_seq s) ->
+      Psuffix s ->
       Pcsteque (Small s).
-  (* Arguments P_Small {lvl s}. *)
-
   Variable P_Big : 
     forall {lvl len nlvl : nat} {C1 C2 C3 : color} {pkt : packet A lvl len C1} 
            {cs : csteque A nlvl C2} {eq : nlvl = len + lvl} {cc : csteque_color C1 C2 C3},
       Ppacket pkt ->
       Pcsteque cs ->
       Pcsteque (Big pkt cs eq cc).
-  (* Arguments P_Big {lvl len nlvl C1 C2 C3 pkt cs eq cc}. *)
 
   (* Fixpoint list_forallt {A : Type} {P : A -> Type} (f : forall (a : A), P a) (l : list A) : Forallt P l :=
     match l with 
     | [] => Forallt_nil P
     | x :: l => Forallt_cons P (f x) (list_forallt f l)
     end. *)
+  Variable deque_rectP_magic : forall {lvl : nat} (d : deque (element A lvl)), Pdeque d.
+  Variable suffix'_rectP_magic : forall {lvl : nat} (s : suffix A lvl), Psuffix s.
 
-  Fixpoint element_rect {lvl : nat} (e : element A lvl) {struct e} : Pelement e :=
+  Fixpoint element_rectP {lvl : nat} (e : element A lvl) : Pelement e :=
+    let prefix'_rectP {lvl : nat} {C : color} (p : prefix A lvl C) := 
+      match p with
+      | Pre2 e1 e2 => P_Pre2 (element_rectP e1) (element_rectP e2)
+      | Pre3 e1 e2 e3 => P_Pre3 (element_rectP e1) (element_rectP e2) (element_rectP e3)
+      | Pre4 e1 e2 e3 e4 d => 
+        P_Pre4 (element_rectP e1) (element_rectP e2) (element_rectP e3) (element_rectP e4) (deque_rectP_magic d)
+      end
+    in
     match e with 
     | ZElt a => P_ZElt a
-    | SElt p cs => 
-      P_SElt (list_forallt (prefix'_seq p)) (csteque_rect cs)
+    | SElt p cs => P_SElt (prefix'_rectP p) (csteque_rectP cs)
     end
 
-  with packet_rect {lvl len : nat} {C : color} (pkt : packet A lvl len C) {struct pkt} : Ppacket pkt :=
+  with packet_rectP {lvl len : nat} {C : color} (pkt : packet A lvl len C) : Ppacket pkt :=
+    let prefix'_rectP {lvl : nat} {C : color} (p : prefix A lvl C) := 
+      match p with
+      | Pre2 e1 e2 => P_Pre2 (element_rectP e1) (element_rectP e2)
+      | Pre3 e1 e2 e3 => P_Pre3 (element_rectP e1) (element_rectP e2) (element_rectP e3)
+      | Pre4 e1 e2 e3 e4 d => 
+        P_Pre4 (element_rectP e1) (element_rectP e2) (element_rectP e3) (element_rectP e4) (deque_rectP_magic d)
+      end
+    in
     match pkt with 
     | Hole => P_Hole
-    | Triple p pkt s => 
-      P_Triple (list_forallt (prefix'_seq p))
-               (packet_rect pkt) 
-               (list_forallt (suffix'_seq s))
+    | Triple p pkt (Suff d) => P_Triple (prefix'_rectP) (packet_rectP pkt) (P_suff (s)
     end
 
-  with csteque_rect {lvl : nat} {C : color} (cs : csteque A lvl C) {struct cs} : Pcsteque cs :=
+  with csteque_rectP {lvl : nat} {C : color} (cs : csteque A lvl C) : Pcsteque cs :=
     match cs with 
-    | Small s => P_Small (list_forallt (suffix'_seq s))
-    | Big pkt cs eq cc => P_Big (packet_rect pkt) (csteque_rect cs)
-    end
-    
-  with list_forallt {lvl : nat} (l : list (element A lvl)) {struct l} : Forallt Pelement l :=
-    match l with 
-    | [] => Forallt_nil Pelement
-    | e :: l => Forallt_cons Pelement (element_rect e) (list_forallt l)
+    | Small s => P_Small (suffix'_rectP_magic s)
+    | Big pkt cs eq cc => P_Big (packet_rectP pkt) (csteque_rectP cs)
     end.
